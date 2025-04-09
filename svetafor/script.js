02dea7367f3bea497eb6c3f1130a97b082da876f
hurshidbe <egamberdiyevx8@gmail.com>
2025-04-10 03:41:48 +0500

    yangi loyihachani boshladim

--- svetafor/script.js
+++ svetafor/script.js
@@ -1,6 +1,9 @@
 const red = document.querySelector(".red");
 const orange = document.querySelector(".orange");
 const green = document.querySelector(".green");
+const text1 = document.querySelector(".texts1");
+const text2 = document.querySelector(".texts2");
+const text3 = document.querySelector(".texts3");
 
 // Qizilni birinchi yoqamiz
 red.style.opacity = 1;
@@ -11,20 +14,29 @@ setInterval(() => {
   red.style.opacity = 1;
   orange.style.opacity = 0.2;
   green.style.opacity = 0.2;
-}, 11000); // har 11 sekundda qaytadan
+  text2.style.opacity = 0;
+  text3.style.opacity = 0;
+  text1.style.opacity = 1;
+}, 11000);
 
 setTimeout(() => {
   setInterval(() => {
     red.style.opacity = 0.2;
     orange.style.opacity = 1;
     green.style.opacity = 0.2;
+    text3.style.opacity = 0;
+    text1.style.opacity = 0;
+    text2.style.opacity = 1;
   }, 11000);
-}, 5000); // 5 sekunddan keyin sariq yoqiladi
+}, 5000);
 
 setTimeout(() => {
   setInterval(() => {
     red.style.opacity = 0.2;
     orange.style.opacity = 0.2;
     green.style.opacity = 1;
+    text1.style.opacity = 0;
+    text2.style.opacity = 0;
+    text3.style.opacity = 1;
   }, 11000);
-}, 7000); // 5 + 2 = 7 sekunddan keyin yashil yoqiladi
+}, 7000);
