46ad31f70d37341250b03650b25e413d82d9cc9b
hurshidbe <egamberdiyevx8@gmail.com>
2025-04-17 16:14:52 +0500

    loyihamga midlleware qoshishni boshladim

--- 4152025/src/app.controller.ts
+++ 4152025/src/app.controller.ts
@@ -5,7 +5,7 @@ import { booksService } from './books/books.service';
 @Controller()
 export class AppController {
   constructor(
-    private readonly appService: AppService,
+    private readonly appService: AppService, // 1 ta constructorga 2 va undan ortiq private ozsak bo'ladi
     private booksService: booksService,
   ) {}
 }
