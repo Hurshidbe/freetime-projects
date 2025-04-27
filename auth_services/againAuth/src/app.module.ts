37c538e0bdf67abfa47dab337e30c95225bdf647
hurshidbe <egamberdiyevx8@gmail.com>
2025-04-27 10:39:10 +0500

    kerakli package lar ornatildi va database ulandi

--- auth_services/againAuth/src/app.module.ts
+++ auth_services/againAuth/src/app.module.ts
@@ -1,11 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { PostsModule } from './posts/posts.module';
+import { UsersModule } from './modules/users/users.module';
+import { PostsModule } from './modules/posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, PostsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    UsersModule,
+    PostsModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (service: ConfigService) => {
+        return {
+          type: 'postgres',
+          autoLoadEntities: true,
+          synchronize: true,
+          host: service.get('host'),
+          port: service.get('db_port'),
+          password: service.get('password'),
+          database: service.get('database'),
+          username: 'postgres',
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
